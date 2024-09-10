7a74145771b612776a20ea2a256ab970fc183bbb
Slycooper1911 <italojosueb@gmail.com>
2024-09-10 10:21:10 -0300

    de novo 4

--- Meu Game 3D/Assets/Scenes/scripts/Player.cs
+++ Meu Game 3D/Assets/Scenes/scripts/Player.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Jogador: MonoBehaviour
 {
-    public int velocidade = 10;
-    public int forcaPulo = 7;
-    public bool noChao;
+    public int velocidade = 5;
+    public int forcaPulo = 10;
+    public bool noChao = false;
     private Rigidbody rb;
+    private AudioSource source;
+    
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(message: "START");
         TryGetComponent(out rb);
+        TryGetComponent(out source);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,28 +32,21 @@ public class Jogador: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(message: "UPDATE");
+        Debug.Log(message: "update");
         float h = Input.GetAxis("Horizontal"); // -1 esquerda,0 naa, 1 direita
         float v = Input.GetAxis("Vertical");// -1 pra tras, 0 nada, 1 pra frente
         
         
       Vector3 direcao = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
-
-        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        rb.AddForce(direcao * (velocidade * Time.deltaTime), ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true) //PULO 
         {
+            source.Play();
             rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
             noChao = false;
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        if (transform.position.y <= -10)
+
+        if (transform.position.y <= -5)
         {
             // jogador caiu
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
